c71f1d8779e9a5b8408589651e1a85badbbebaff
srushti engineer <srushtie123@gmail.com>
2025-04-13 22:32:39 +0530

    layouts

--- AndroidDevLearningProject/app/src/main/java/com/example/learningproject/ConstraintChainAndGroupActivity.kt
+++ AndroidDevLearningProject/app/src/main/java/com/example/learningproject/ConstraintChainAndGroupActivity.kt
@@ -1,36 +1,20 @@
 package com.example.learningproject
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.learningproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
+class ConstraintChainAndGroupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_constraint_chain_and_group)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.btnGoToWidgets.setOnClickListener {
-            val intent = Intent(this, WidgetsHomeActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.btnGoToLayouts.setOnClickListener {
-            val intent = Intent(this, LayoutsHomeActivity::class.java)
-            startActivity(intent)
         }
     }
 }
